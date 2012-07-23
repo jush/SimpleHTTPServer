a41ea4a129fbca40f670db25c870203502e0c558
Ramon <oni.pool@gmail.com>
2012-07-23 14:12:25 +0300

    Initial commit

---
+++ webserver.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+import string,cgi,time
+from os import curdir, sep
+from BaseHTTPServer import HTTPServer
+from SimpleHTTPServer import SimpleHTTPRequestHandler
+
+class MyHandler(SimpleHTTPRequestHandler):
+
+    def do_POST(self):
+        global rootnode
+        try:
+            ctype, pdict = cgi.parse_header(self.headers.getheader('content-type'))
+            if ctype == 'multipart/form-data':
+                query=cgi.parse_multipart(self.rfile, pdict)
+            self.send_response(301)
+            
+            self.end_headers()
+            upfilecontent = query.get('upfile')
+            print "filecontent", upfilecontent[0]
+            self.wfile.write("<HTML>POST OK.<BR><BR>");
+            self.wfile.write(upfilecontent[0]);
+            
+        except :
+            pass
+
+def main():
+    try:
+        server = HTTPServer(('', 8000), MyHandler)
+        print 'started httpserver...'
+        server.serve_forever()
+    except KeyboardInterrupt:
+        print '^C received, shutting down server'
+        server.socket.close()
+
+if __name__ == '__main__':
+    main()
+
